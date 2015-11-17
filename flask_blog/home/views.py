62717a0ddf3a82e0321d21fa2afaab7e80310a86
Libert R Schmidt <katari@waynasolutions.com>
2015-11-17 15:08:09 -0500

    Edit|Delete

--- flask_blog/home/views.py
+++ flask_blog/home/views.py
@@ -17,7 +17,7 @@ POST_PER_PAGE = 5
 def index(page=1):
 
     blog = Blog.query.first()
-    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, POST_PER_PAGE, False)
+    posts = Post.query.filter_by(live=True).order_by(Post.publish_date.desc()).paginate(page, POST_PER_PAGE, False)
     return render_template('blog/index.html', blog=blog, posts=posts)
 
 
@@ -27,7 +27,7 @@ def index(page=1):
 @author_required
 
 def admin(page=1):
-    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, POST_PER_PAGE, False)
+    posts = Post.query.filter_by(live=True).order_by(Post.publish_date.desc()).paginate(page, POST_PER_PAGE, False)
 
     return render_template('blog/admin.html',posts=posts)
 
@@ -83,7 +83,7 @@ def post():
             filename = uploaded_images.save(image)
 
         except:
-            flash("The Image was npt uploaded")
+            flash("The Image was not uploaded")
 
         if form.new_category.data:
             new_category = Category(form.new_category.data)
@@ -101,7 +101,51 @@ def post():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('article', slug=slug))
-    return render_template('blog/post.html', form=form)
+    return render_template('blog/post.html', form=form, action='new')
+
+
+
+@app.route('/edit/<int:post_id>', methods = ('GET', 'POST'))
+@author_required
+
+def edit(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    form = PostForm(obj=post)
+    if form.validate_on_submit():
+        original_image = post.image
+        form.populate_obj(post)
+        if form.image.has_file():
+            image = request.files.get('image')
+            try:
+                filename = uploaded_images.save(image)
+            except:
+                flash('The image was not uploaded!')
+
+            if filename:
+                post.image = filename
+
+        else:
+            post.image = original_image
+
+        if form.new_category.data:
+            new_category = Category(form.new_category.data)
+            db.session.add(new_category)
+            db.session.flush()
+            post.category = new_category
+        db.session.commit()
+        return redirect(url_for('article', slug=post.slug))
+    return render_template('blog/post.html', form=form, post=post, action='edit')
+
+
+@app.route('/delete/<int:post_id>', methods = ('GET', 'POST'))
+@author_required
+
+def delete(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    post.live = False
+    db.session.commit()
+    flash('Article deleted')
+    return redirect(url_for('admin'))
 
 @app.route('/article/<slug>')
 def article(slug):
