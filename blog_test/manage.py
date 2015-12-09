63ccfbbfce3814915238c245f52177a4ee0d66c9
Libert R Schmidt <katari@waynasolutions.com>
2015-12-08 22:04:56 -0500

    test|coverage

--- blog_test/manage.py
+++ blog_test/manage.py
@@ -1,7 +1,8 @@
+import os
+import unittest
+import coverage
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-import os
-
 from project import app, db
 
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -9,6 +10,29 @@ migrate = Migrate(app,db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    tests = unittest.TestLoader().discover('.')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    cov = coverage.coverage(branch=True, include='project/*')
+    cov.start()
+    tests = unittest.TestLoader().discover('.')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    cov.stop()
+    cov.save()
+    print 'Coverage Summary:'
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'coverage')
+    cov.html_report(directory=covdir)
+    cov.erase()
+
 
 if __name__ == '__main__':
     manager.run()
